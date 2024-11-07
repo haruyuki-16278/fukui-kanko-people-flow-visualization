7ba481339a6014e1d0d64432f351ad99ed611d3e
yamaji <yamaji@jig.jp>
2024-11-07 13:19:33 +0900

    🚑 pagesのpathが参考元のままになっていたのを修正

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.PAGES ? "/hokuriku-inbound-kanko-visualization" : "",
+  basePath: process.env.PAGES ? "/fukui-kanko-people-flow-visualization" : "",
 };
 
 export default nextConfig;
