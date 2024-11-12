502e6a053e236c1fd2f512da2164f2b49778bd45
yamaji <yamaji@jig.jp>
2024-11-12 10:25:14 +0900

    👍 グラフの表示順を修正

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default async function Home() {
       <article className="mb-4 flex flex-col items-center p-4">
         <h2 className="mb-2 text-xl font-bold">福井駅でのAIカメラによる解析</h2>
         <div className="grid h-full w-full grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
-          <DetectedFaceGraph place={"fukui-terminal"} date={date} />
           <DetectedPeopleGraph place={"fukui-terminal"} date={date} />
+          <DetectedFaceGraph place={"fukui-terminal"} date={date} />
           <EstimatedAgeGraph place={"fukui-terminal"} date={date} />
           <EstimatedGenderGraph place={"fukui-terminal"} date={date} />
         </div>
@@ -22,8 +22,8 @@ export default async function Home() {
       <article className="mb-4 flex flex-col items-center p-4">
         <h2 className="mb-2 text-xl font-bold">東尋坊でのAIカメラによる解析</h2>
         <div className="grid h-full w-full grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
-          <DetectedFaceGraph place={"tojinbo"} date={date} />
           <DetectedPeopleGraph place={"tojinbo"} date={date} />
+          <DetectedFaceGraph place={"tojinbo"} date={date} />
           <EstimatedAgeGraph place={"tojinbo"} date={date} />
           <EstimatedGenderGraph place={"tojinbo"} date={date} />
         </div>
