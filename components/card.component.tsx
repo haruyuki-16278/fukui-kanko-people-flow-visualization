696a7bfccacfcafe031ad85acc23a06348d60e3a
yamaji <yamaji@jig.jp>
2024-12-24 10:57:26 +0900

    👍 Cardコンポーネントにiマークとツールチップを表示できるように

--- components/card.component.tsx
+++ components/card.component.tsx
@@ -1,9 +1,11 @@
+import { InfoIcon } from "@primer/octicons-react";
 import { ReactNode } from "react";
 
 type Props = {
   title: string;
   children: ReactNode;
   className?: string;
+  information?: string;
 };
 
 export function Card(props: Props) {
@@ -11,7 +13,17 @@ export function Card(props: Props) {
     <article
       className={`flex h-fit flex-col items-center rounded-lg border-2 border-primary bg-background p-4 ${props.className}`}
     >
-      <h3 className="mb-4 text-lg font-bold">{props.title}</h3>
+      <div className="grid-cols-edgetip grid h-12 w-full grid-rows-1 place-items-center">
+        <div></div>
+        <h3 className="h-fit w-fit text-center text-lg font-bold">{props.title}</h3>
+        {props.information ? (
+          <div className="tooltip group" data-tooltip={props.information}>
+            <InfoIcon size={"small"} className="fill-gray-500 group-hover:scale-110" />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
       {props.children}
     </article>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { PluginCreator } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
@@ -13,8 +14,41 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      gridTemplateColumns: {
+        edgetip: "64px 1fr 64px",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    ((api) => {
+      api.addUtilities({
+        ".tooltip": {
+          position: "relative",
+          display: "inline-block",
+          cursor: "pointer",
+          "&::before": {
+            content: "attr(data-tooltip)",
+            position: "absolute",
+            bottom: "100%",
+            left: "50%",
+            transform: "translateX(-50%)",
+            "background-color": "#333",
+            color: "white",
+            padding: "5px",
+            "border-radius": "5px",
+            "font-size": "12px",
+            "white-space": "nowrap",
+            opacity: "0",
+            visibility: "hidden",
+            transition: "opacity 0.3s",
+          },
+          "&:hover::before": {
+            opacity: "1",
+            visibility: "visible",
+          },
+        },
+      });
+    }) as PluginCreator,
+  ],
 };
 export default config;
