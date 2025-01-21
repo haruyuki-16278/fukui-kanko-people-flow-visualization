2caea0c5ea3475b38b5b6674b1c0eef708b18952
yamaji <yamaji@jig.jp>
2025-01-21 09:09:55 +0900

    👍 ページのレイアウトを刷新

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,25 +20,29 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="data:image/x-icon;," />
       </head>
-      <body className="flex min-h-screen flex-col items-center justify-center p-4 antialiased">
-        <header className="border-separator flex h-12 w-full items-center justify-between gap-x-2 border-b-2 pb-2">
-          <Link
-            className="group flex h-fit w-fit items-center justify-start gap-x-2 no-underline"
-            href="/"
-            rel="noopener noreferrer"
-          >
-            <GraphIcon
-              size="medium"
-              verticalAlign="top"
-              className="group-hover:scale-120 fill-primary transition-all"
-            />
-            <h1 className="text-2xl font-bold transition-all group-hover:scale-110 group-hover:underline">
-              福井観光DX:グラフ
-            </h1>
-          </Link>
-          <ExternalNavigaton />
-        </header>
-        <main className="flex h-full w-full flex-grow flex-col items-center pt-4">{children}</main>
+      <body className="h-full w-full antialiased">
+        <div className="flex min-h-screen w-full flex-col items-center justify-center p-4">
+          <header className="border-separator flex h-12 w-full items-center justify-between gap-x-2 border-b-2 pb-2">
+            <Link
+              className="group flex h-fit w-fit items-center justify-start gap-x-2 no-underline"
+              href="/"
+              rel="noopener noreferrer"
+            >
+              <GraphIcon
+                size="medium"
+                verticalAlign="top"
+                className="group-hover:scale-120 fill-primary transition-all"
+              />
+              <h1 className="text-2xl font-bold transition-all group-hover:scale-110 group-hover:underline">
+                福井観光DX:グラフ
+              </h1>
+            </Link>
+            <ExternalNavigaton />
+          </header>
+          <main className="flex h-full w-full max-w-full flex-grow items-center pt-4">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
