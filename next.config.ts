216e963ef2ccc151d76656399aa2c256dc136336
yamaji <yamaji@jig.jp>
2025-01-23 18:50:30 +0900

    👍 assetPrefixを追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.PAGES ? "/fukui-kanko-people-flow-visualization" : "",
+  assetPrefix: process.env.PAGES ? "/fukui-kanko-people-flow-visualization" : "",
 };
 
 export default nextConfig;
