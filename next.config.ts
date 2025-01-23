7e4bbcd41425f7407841ffce93016849bec36d47
yamaji <yamaji@jig.jp>
2025-01-23 18:54:54 +0900

    👍 trailingSlashを追加

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.PAGES ? "/fukui-kanko-people-flow-visualization" : "",
   assetPrefix: process.env.PAGES ? "/fukui-kanko-people-flow-visualization" : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
