55513453e71d0ccfcf61af921964224d1ed15ca8
yamaji <yamaji@jig.jp>
2025-01-23 13:31:42 +0900

    👍 一部のみをRequiredにする型関数を定義

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export type PartiallyRequired<T, K extends keyof T> = T & {
+  [P in K]-?: T[P];
+};
