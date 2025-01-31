8b01a7ce677304be51e92ce44dbe7ddc11d2d0a3
yamaji <yamaji@jig.jp>
2025-01-31 21:27:23 +0900

    👍 pnpm build が通過するように修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import App from "@/App";
+import { Header } from "@/components/parts/header.component";
+import "@/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
-import { Header } from "./components/parts/header.component";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
