ee96cc0a6790924fc93a0d393239cae3aeb3a051
kuwabara <kuwabara@jig.jp>
2025-06-04 11:29:49 +0900

    👍 RegionCheckboxコンポーネントを追加

---
+++ src/components/parts/region-checkbox.component.tsx
@@ -0,0 +1,64 @@
+import { Checkbox } from "@/components/ui/checkbox";
+import { GraphSeries } from "@/interfaces/graph-series.interface";
+
+interface RegionCheckboxProps {
+  region: { name: string; prefectures: string[] };
+  objectClassAttribute: string;
+  series: GraphSeries;
+  notify: (series: GraphSeries) => void;
+  updateSeriesProperty: <Key extends keyof GraphSeries>(
+    [key, value]: [Key, GraphSeries[Key]],
+    series: GraphSeries,
+  ) => GraphSeries;
+}
+
+export function RegionCheckbox({
+  region,
+  objectClassAttribute,
+  series,
+  notify,
+  updateSeriesProperty,
+}: RegionCheckboxProps) {
+  // 地方に属する都道府県が全て表示されているかチェック
+  const allChecked = region.prefectures.every(
+    (prefectureKey) => !series.exclude?.[objectClassAttribute]?.includes(prefectureKey),
+  );
+
+  // 地方に属する都道府県が一つでも表示されているかチェック
+  const anyChecked = region.prefectures.some(
+    (prefectureKey) => !series.exclude?.[objectClassAttribute]?.includes(prefectureKey),
+  );
+
+  // チェックボックスの状態を決定
+  const checkState = allChecked ? true : anyChecked ? "indeterminate" : false;
+
+  return (
+    <Checkbox
+      checked={checkState}
+      onCheckedChange={(v) =>
+        notify(
+          updateSeriesProperty(
+            [
+              "exclude",
+              {
+                ...series.exclude,
+                [objectClassAttribute]: v
+                  ? (series.exclude?.[objectClassAttribute] ?? []).filter(
+                      (prefectureKey) => !region.prefectures.includes(prefectureKey),
+                    )
+                  : [
+                      ...(series.exclude?.[objectClassAttribute] ?? []),
+                      ...region.prefectures.filter(
+                        (prefectureKey) =>
+                          !series.exclude?.[objectClassAttribute]?.includes(prefectureKey),
+                      ),
+                    ],
+              },
+            ],
+            series,
+          ),
+        )
+      }
+    />
+  );
+}
