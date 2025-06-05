133aef6e29160528c6f818b13f0d74ac7a710720
kuwabara <kuwabara@jig.jp>
2025-06-05 09:40:10 +0900

    👍 全選択チェックボックスの状態管理を関数に分離

--- src/components/parts/all-checkbox.component.tsx
+++ src/components/parts/all-checkbox.component.tsx
@@ -1,7 +1,7 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { GraphSeries } from "@/interfaces/graph-series.interface";
 
-interface AllCheckboxProps {
+interface Props {
   attributeKey: string;
   itemKey: string[];
   series: GraphSeries;
@@ -12,30 +12,41 @@ interface AllCheckboxProps {
   ) => GraphSeries;
 }
 
+export function handleAllCheckboxChange(
+  series: GraphSeries,
+  attributeKey: string,
+  itemKey: string[],
+  isChecked: boolean,
+  updateSeriesProperty: <Key extends keyof GraphSeries>(
+    [key, value]: [Key, GraphSeries[Key]],
+    series: GraphSeries,
+  ) => GraphSeries,
+): GraphSeries {
+  return updateSeriesProperty(
+    [
+      "exclude",
+      isChecked
+        ? { ...series.exclude, [attributeKey]: [] } // 全チェックON
+        : {
+            ...series.exclude,
+            [attributeKey]: itemKey,
+          }, // 全チェックOFF
+    ],
+    series,
+  );
+}
+
 export function AllCheckbox({
   attributeKey,
   itemKey,
   series,
   notify,
   updateSeriesProperty,
-}: AllCheckboxProps) {
+}: Props) {
   return (
     <Checkbox
       onCheckedChange={(v) =>
-        notify(
-          updateSeriesProperty(
-            [
-              "exclude",
-              v
-                ? { ...series.exclude, [attributeKey]: [] } // 全チェックON
-                : {
-                    ...series.exclude,
-                    [attributeKey]: itemKey,
-                  }, // 全チェックOFF
-            ],
-            series,
-          ),
-        )
+        notify(handleAllCheckboxChange(series, attributeKey, itemKey, !!v, updateSeriesProperty))
       }
       className="block"
       checked={!series.exclude?.[attributeKey]?.length}
