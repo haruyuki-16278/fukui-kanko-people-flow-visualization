80e204e00975f68a9665a74a7df2f78b0641e00b
kuwabara <kuwabara@jig.jp>
2025-06-11 16:56:12 +0900

    ♻️ ツールチップの z-index をカスタム CSS 変数に変更し、Tailwind CSS 設定に追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,9 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      zIndex: {
+        tooltip: "var(--tooltip-z-index)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
