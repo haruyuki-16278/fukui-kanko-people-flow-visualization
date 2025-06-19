7bf5d1d120a0a0b290f7f3da393055a25b7ecc60
kuwabara <kuwabara@jig.jp>
2025-06-19 10:58:26 +0900

    👍 ローディングスピナーのサイズとスタイルを改善

--- src/components/parts/loading-spinner.component.tsx
+++ src/components/parts/loading-spinner.component.tsx
@@ -1,8 +1,8 @@
 export function LoadingSpinner() {
   return (
     <div className="flex flex-col items-center justify-center fixed inset-0 bg-gray-500 bg-opacity-50">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
-      <p className="mt-4 text-lg">データを読み込み中...</p>
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-primary"></div>
+      <p className="mt-4 text-2xl font-bold">データを読み込み中...</p>
     </div>
   );
 }
