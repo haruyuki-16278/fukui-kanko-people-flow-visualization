9a3625ccc5aa0c9244fec76db6f85459023710bb
yamaji <yamaji@jig.jp>
2025-07-11 10:48:07 +0900

    refactor: 複数アプリ対応のモノレポ構成に移行
    - 既存アプリをpackages/people-flow-viz/に移動
    - 共有リソース用のpackages/shared/を追加
    - pnpm workspaceでの管理に変更
    - GitHub Actionsをモノレポ対応に更新

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,14 @@
+// 共有コンポーネント
+export * from './components';
+
+// 共有ユーティリティ
+export * from './utils';
+
+// 共有フック
+export * from './hooks';
+
+// 共有型定義
+export * from './types';
+
+// 共有定数
+export * from './constants';
